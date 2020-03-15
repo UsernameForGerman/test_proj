7da6d08f422bdf15b5d9d3df8074d9bed90e60ac
German <polyakgerman@gmail.com>
2020-03-16 00:01:27 +0300

    debug off

--- test_proj/settings.py
+++ test_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#o0r^*^bwdmf-iw_1cl_dg$n3a2_2zq+39tsjka+f%xf)vfgvc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 DOCKER = True
 
 ALLOWED_HOSTS = ['*']
